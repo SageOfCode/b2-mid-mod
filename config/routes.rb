8916aaf96b95dcaf6ab67432669aa1de92fc5e36
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-23 11:25:13 -0600

    Add route for mechanic show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   #mechanics
   get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
 
   #mechanics
   get '/amusement_parks/:id', to: 'amusement_parks#show'
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,32 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor" do
   describe "When I visit a mechanics show page" do
-    it 'I see a header saying “All Mechanics”' do
+    it 'I see their name, years of experience, 
+    and names of all rides they’re working on
+    And I also see a form to add a ride to their workload
+    When I fill in that field with an id of a ride that exists 
+    in the database
+    And I click submit
+    I’m taken back to that mechanics show page
+    And I see the name of that newly added ride 
+    on this mechanics show page' do
       
       mike = Mechanic.create!(
         name: "Mike Carman",
         years_of_experience: 7
       )
 
-      visit '/mechanics'
+      tower = Ride.create!( 
+        name: "Tower of Doom",  
+        amusement_park_id: elitch.id,
+        thrill_rating: 10
+      )
+      twister = Ride.create!( 
+        name: "Twister 2",
+        amusement_park_id: elitch.id,
+        thrill_rating: 8
+      )
+
+      visit "/mechanics/#{mike.id}"
 
       expect(page).to have_content("All Mechanics")
-    end
-    it 'I see a header saying “All Mechanics”' do
-      
-      mike = Mechanic.create!(
-        name: "Mike Carman",
-        years_of_experience: 7
-      )
-      efren = Mechanic.create!(
-        name: "Efren DelCarro",
-        years_of_experience: 10
-      )
-
-      visit '/mechanics'
-
-      expect(page).to have_content("Mike Carman: 7 Years of Experience")
-      expect(page).to have_content("Efren DelCarro: 10 Years of Experience")
     end
   end
 end
