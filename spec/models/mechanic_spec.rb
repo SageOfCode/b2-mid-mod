f9fadce5573afb0311ecdcc637b95c826aa4c3d3
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-23 11:51:14 -0600

    Add tests for all models

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Mechanic, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :years_of_experience}
   end
+
+  describe "validations" do
+    it {should have_many(:ride_mechanics)}
+    it {should have_many(:rides).through(:ride_mechanics)}
+  end
+  
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :thrill_rating}
+  end
+
+  describe "validations" do
+    it {should have_many(:ride_mechanics)}
+    it {should have_many(:mechanics).through(:ride_mechanics)}
+  end
 end
